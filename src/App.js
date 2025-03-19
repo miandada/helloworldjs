54b138b8177f6471f383e278c9310f25d5ea03c4
miandada <35865135+miandada@users.noreply.github.com>
2025-03-19 17:55:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, World! Main</h1>
+        <h1>Prueba auto CI CD</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
